9d3deaea66e18445b3613a0316328382fc7f250b
osorio1985 <albertopeto2009@gmail.com>
2023-05-25 00:31:28 -0600

    opload first

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import WeatherAPPContext from './Context/Context';
+import dayjs from 'dayjs';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <WeatherAPPContext>
+      <App />
+    </WeatherAPPContext>
+  </React.StrictMode>
+);
+
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
